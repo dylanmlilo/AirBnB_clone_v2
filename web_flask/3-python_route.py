c71023f2a0c2b4aa9ef314b683424e122fe4f3b6
dylanmlilo <dylanmlilo12@gmail.com>
2024-02-17 05:14:52 -0800

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ displays Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ displays HBNB! """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_C_text(text):
+    """
+    andles the '/c/<text>' path, replacing underscores with spaces
+    and displaying 'C ' followed by the text
+    """
+    # Replace underscores with spaces in the text variable
+    text_with_spaces = text.replace("_", " ")
+
+    # Combine "C " and the text with spaces
+    display_text = "C " + text_with_spaces
+
+    return display_text
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_py_text(text="is cool"):
+    """
+    Handles the '/python/<text>' path, replacing underscores with spaces
+    and displaying 'Python ' followed by the text with "is cool" as the default
+    text
+    """
+    # Replace underscores with spaces in the text variable
+    text_with_spaces = text.replace("_", " ")
+
+    # Combine "Python " and the text with spaces
+    display_text = "Python " + text_with_spaces
+
+    return display_text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
