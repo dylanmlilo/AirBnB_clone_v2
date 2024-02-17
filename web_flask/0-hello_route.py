f52bf245a490fef0c5475a2517bff4f428c7603f
dylanmlilo <dylanmlilo12@gmail.com>
2024-02-17 04:17:55 -0800

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application """
 from flask import Flask
-
-
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
+""" displays Hello HBNB! """
 def hello_hbnb():
     return "Hello HBNB!"
 
